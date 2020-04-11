117f52450411ae9a695706f10d61fc72777034bc
Liam Miller <liamcharlesmiller@gmail.com>
2020-04-11 18:38:09 +0100

    Updating Widget Data (Or: Using StatelessWidget Incorrectly)

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -5,8 +5,11 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+
   void answerQuestion() {
-    print ('Answer Chosen');
+    questionIndex = questionIndex + 1;
+    print (questionIndex);
   }
 
   @override
@@ -22,7 +25,7 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text('The question!'),
+            Text(questions[questionIndex]),
             RaisedButton(
               child: Text('Answer 1'),
               onPressed: answerQuestion,
